90c33d47bed5ed510ccdbe4a01678b02d4c9272c
NaufalArdian12 <naufalopo87@gmail.com>
2024-09-26 08:04:16 +0700

    sign in or sign up

--- lib/presentation/auth/pages/signup_or_signin.dart
+++ lib/presentation/auth/pages/signup_or_signin.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotifyapp/common/helpers/is_dark_mode.dart';
+import 'package:spotifyapp/common/widgets/button/basic_app_button.dart';
 import 'package:spotifyapp/core/configs/assets/app_images.dart';
 import 'package:spotifyapp/core/configs/assets/app_vectors.dart';
 
@@ -32,12 +34,74 @@ class SignupOrSigninPage extends StatelessWidget {
 
           Align(
             alignment: Alignment.center,
-            child: Column(
-              children: [
-                SvgPicture.asset(
-                  AppVectors.logo
-                )
-              ],
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 40
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    AppVectors.logo
+                  ),
+                  SizedBox(
+                    height: 48,
+                  ),
+                  Text(
+                    'Enjoy Listening To Music',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Text(
+                      'Spotify is a proprietary Swedish audio streaming and media services provider ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+              
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: BasicAppButton(
+                          onPressed: (){}, 
+                          title: 'Register',
+                        ),
+                      ),
+                      SizedBox(width: 24,),
+                      Expanded(
+                        flex: 1,
+                        child: TextButton(
+                          onPressed: (){}, 
+                          child: Text(
+                            'Sign in',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: context.isDarkMode ? Colors.white : Colors.black
+                            ),
+                          )
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
             )
           )
         ],
